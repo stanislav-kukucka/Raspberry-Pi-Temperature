5ad9f8d0f4938a3fd7e4cb636f1f585ab774a7b0
Stanislav Kukucka <89223349+stanislav-kukucka@users.noreply.github.com>
2023-12-28 12:14:27 +0100

    Update raspberry-temp.py

--- raspberry-temp.py
+++ raspberry-temp.py
@@ -4,18 +4,19 @@ import time
 
 while True:
     try:
-        # Spustíme príkaz vcgencmd measure_temp a získame výsledok
+        # Spustím príkaz "vcgencmd measure_temp" a získam výsledok
         result = subprocess.check_output(["vcgencmd", "measure_temp"]).decode("utf-8")
 
-        # Analyzujeme výsledok pomocou regulárneho výrazu na získanie teploty
+        # Analyzujem výsledok pomocou regulárneho výrazu na získanie teploty
         match = re.search(r'temp=(\d+\.\d+)', result)
         if match:
             cpu_temp = round(float(match.group(1)), 1)
             print("Teplota CPU je " + str(cpu_temp) + "°C")
         else:
             print("Nepodarilo sa získať teplotu.")
-
-        time.sleep(1)  # Pauza 1 sekunda medzi každým meraním teploty
+            
+        # Pauza 1 sekunda medzi každým meraním teploty
+        time.sleep(1)  
     except KeyboardInterrupt:
         print("Program ukončený.")
         break
